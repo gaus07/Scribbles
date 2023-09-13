6f1914a9e16be433ffb840001d982a66fd7bf1e1
gaus07 <gausnoori07@gmail.com>
2023-09-13 23:50:46 +0530

    Comment Auth

--- server/routes/Users.js
+++ server/routes/Users.js
@@ -1,30 +1,37 @@
-const express = require('express')
-const router = express.Router()
-const {Users} = require('../models')
-const bcrypt = require('bcrypt')
+const express = require("express");
+const router = express.Router();
+const { Users } = require("../models");
+const bcrypt = require("bcrypt");
 
-router.post('/', async (req, res) => {
-    const {username, password} = req.body
-    bcrypt.hash(password, 10).then((hash) => {
-        Users.create({
-            username: username,
-            password: hash
-        })
-        return res.json('Success')
-    })
-})
+const { sign } = require("jsonwebtoken");
 
-router.post('/login', async (req, res) => {
-    const {username, password} = req.body
+router.post("/", async (req, res) => {
+  const { username, password } = req.body;
+  bcrypt.hash(password, 10).then((hash) => {
+    Users.create({
+      username: username,
+      password: hash,
+    });
+    return res.json("Success");
+  });
+});
 
-    const user = await Users.findOne({where: {username: username}})
+router.post("/login", async (req, res) => {
+  const { username, password } = req.body;
 
-    if (!user) return res.json({error: 'User Does not exist'})
+  const user = await Users.findOne({ where: { username: username } });
 
-    bcrypt.compare(password, user.password).then((match) => {
-        if (!match) return res.json({error: 'Username or Password is wrong'})
-        return res.json('Logged In')
-    })
-})
+  if (!user) return res.json({ error: "User Does not exist" });
+
+  bcrypt.compare(password, user.password).then((match) => {
+    if (!match) return res.json({ error: "Username or Password is wrong" });
+
+    const accessToken = sign(
+      { username: user.username, id: user.id },
+      "importantsecret"
+    );
+    return res.json(accessToken);
+  });
+});
 
 module.exports = router;
